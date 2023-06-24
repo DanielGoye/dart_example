517daca63730974b448a6d267dc232a51af586e1
Daniel Goye <danielgoye92@gmail.com>
2023-06-24 11:22:14 +0300

    Update input page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:bmi_calculator/input_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -22,23 +23,6 @@ class BMICalculator extends StatelessWidget {
           primaryColor: const Color(0xFF0A0E21),
           appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF0A0E21)),
           scaffoldBackgroundColor: const Color(0xFF0A0E21)),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('BMI CALCULATOR')),
-      body: const Center(child: Text('Body Text')),
     );
   }
 }
