e22f932aa421c0e3e2ca202d51609c0fc15b0881
Daniel Goye <danielgoye92@gmail.com>
2023-06-26 09:25:41 +0300

    Add gender enums

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -12,15 +12,13 @@ class ReusableCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: color,
-        ),
-        child: cardChild,
+    return Container(
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: color,
       ),
+      child: cardChild,
     );
   }
 }
