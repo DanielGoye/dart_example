76d5f7bf72e656a10087ca8ec5cb141a6833d164
Daniel Goye <danielgoye92@gmail.com>
2023-06-29 23:19:32 +0300

    Update main_flat_button.dart

--- lib/widgets/main_flat_button.dart
+++ lib/widgets/main_flat_button.dart
@@ -18,6 +18,7 @@ class MainFlatButton extends StatelessWidget {
         color: kButtonColor,
         width: double.infinity,
         height: 80,
+        padding: const EdgeInsets.only(bottom: 15),
         child: Center(
           child: Text(
             text,
