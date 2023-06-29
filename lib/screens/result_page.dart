9158e2c66b9232f4acf5fd47477ec7731169af32
Daniel Goye <danielgoye92@gmail.com>
2023-06-29 22:22:32 +0300

    Design results page

--- lib/screens/result_page.dart
+++ lib/screens/result_page.dart
@@ -1,3 +1,6 @@
+import 'package:bmi_calculator/constants/constants.dart';
+import 'package:bmi_calculator/widgets/main_flat_button.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -6,12 +9,47 @@ class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("RESULTS"),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text("Results Page"),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Your Result",
+              style: kNumberTextStyle.copyWith(fontSize: 40),
+            ),
+            Expanded(
+              child: ReusableCard(
+                  color: kActiveCardColor,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        "OVERWEIGHT",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24),
+                      ),
+                      Text(
+                        "28.4",
+                        style: kNumberTextStyle.copyWith(fontSize: 70),
+                      ),
+                      const Text(
+                        "You have a higher than normal body weight. Try to exercise more",
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  onPressed: () {}),
+            ),
+            MainFlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                text: "RE-CALCULATE"),
+          ],
+        ),
       ),
     );
   }
